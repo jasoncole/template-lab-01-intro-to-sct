08c0ce841b46f3d56c5b0303dbe5df553c63ca2b
jasoncole <mediocrechicken@gmail.com>
2020-01-07 10:20:41 -0800

    Add rectangle area program
    * Added Rectangle class files and main file that calculates the rectangle's area

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "../header/rectangle.hpp"
+
+int main() {
+	Rectangle rect1, rect2;
+	rect.set_width(3);
+	rect.set_height(4);
+
+	rect2.set_width(4);
+	rect2.set_height(2);
+
+	std::cout << "Rectangle 1 area: " << rect1.area() << std::endl;
+	std::cout << "Rectangle 2 area: " << rect2.area() << std::endl;
+	return 0;
+}
